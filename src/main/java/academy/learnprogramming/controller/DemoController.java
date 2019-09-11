d4f9bcd43f923214555e3302be4c3eb92462f6c6
tiagoandrem <tiagoandrem@hotmail.com>
2019-09-11 17:18:42 +0100

    MVC video 16

---
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -0,0 +1,50 @@
+package academy.learnprogramming.controller;
+
+import academy.learnprogramming.services.DemoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class DemoController {
+
+    // == Fields ==
+    private final DemoService demoService;
+
+    // == Constructors ==
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
+
+    // http://localhost:8080/todo-list/hello
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    // http://localhost:8080/todo-list/welcome
+    // http://localhost:8080/todo-list/welcome?user=Tiago
+    @GetMapping("welcome")
+    public String welcome(@RequestParam String user, @RequestParam int age, Model model) {
+        model.addAttribute("helloMessage", demoService.getHelloMessage(user));
+        model.addAttribute("age", age);
+        log.info("model = {}", model );
+        // prefix + name + suffix
+        // /WEB-INF/view/welcome.jsp
+        return "welcome";
+    }
+
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage(){
+        log.info("welcomeMessage() called");
+        return demoService.getWelcomeMessage();
+    }
+}
---
+++ src/main/java/academy/learnprogramming/services/DemoService.java
@@ -0,0 +1,7 @@
+package academy.learnprogramming.services;
+
+public interface DemoService {
+
+    String getHelloMessage(String user);
+    String getWelcomeMessage();
+}
